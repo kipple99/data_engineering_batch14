dc936686c5ce5aecdaa5156ad5f514097da00f63
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-09-06 17:06:43 +0900

    Update NameGenderCSVtoRedshift.py

--- dags/NameGenderCSVtoRedshift.py
+++ dags/NameGenderCSVtoRedshift.py
@@ -7,8 +7,8 @@ import psycopg2
 
 def get_Redshift_connection():
     host = "learnde.cduaw970ssvt.ap-northeast-2.redshift.amazonaws.com"
-    user = "keeyong"  # 본인 ID 사용
-    password = "..."  # 본인 Password 사용
+    user = "sungwoodat99"  # 본인 ID 사용
+    password = "Sungwoodat99!1"  # 본인 Password 사용
     port = 5439
     dbname = "dev"
     conn = psycopg2.connect(f"dbname={dbname} user={user} host={host} password={password} port={port}")
